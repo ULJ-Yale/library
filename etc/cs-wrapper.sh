20e7ef3cc99597447b5f71222dc12063585e347b
Alan Anticevic of group anticevic <aa353@grace2.grace.hpc.yale.internal>
2018-08-16 00:03:53 -0400

    Turnkey updates
    Former-commit-id: 41febc12593294f58ff5aef1dc26bfa59b5e5f5b
    Former-commit-id: 43e38850f986c71a8b82dcdbcb391a3c69b9a75b

--- etc/cs-wrapper.sh
+++ etc/cs-wrapper.sh
@@ -1,2 +1,2 @@
 #!/bin/bash -i
-/opt/mnaptools/connector/functions/RunTurnkey.sh ${@:1} --xnathost=$XNAT_HOST --xnatuser=$XNAT_USER --xnatpass=$XNAT_PASS
+/opt/mnaptools/connector/functions/RunTurnkey.sh ${@:1} --xnathost=${XNAT_HOST} --xnatuser=${XNAT_USER} --xnatpass=${XNAT_PASS}
