abd90207a7da572bc82b0e7671cab41331b69fea
Alan Anticevic of group anticevic <aa353@grace2.grace.hpc.yale.internal>
2018-12-02 13:32:11 -0500

    Added bin general purpose dir in library and Singularity SLURM example code
    Former-commit-id: 3d44a0fa37b8ebc0305b932fea8a4e4e0aa1eb2f
    Former-commit-id: f415261fd7deebd54133a02f1d6124b49e9fa3bf

---
+++ bin/SBATCH_MNAPSingularity.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#SBATCH --partition=day
+#SBATCH --job-name=MNAPSingularity_Commands
+#SBATCH --ntasks=1 --nodes=1
+#SBATCH --mem-per-cpu=15000
+#SBATCH --time=23:59:00
+
+# -- MNAP Singularity Container Image absolute paths
+MNAPCONTAINER=${TOOLS}/Singularity
+PATH=${MNAPCONTAINER}:${PATH}
+export MNAPCONTAINER PATH
+
+# -- Command to execute the shell script with MNAP-specific commands
+CWD=`pwd`
+singularity exec ${MNAPCONTAINER}/mnap_suite.simg ${CWD}/MNAPSingularity_Commands.sh
