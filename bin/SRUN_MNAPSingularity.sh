c487021a4b2df5d292b979e694976811817adb18
Alan Anticevic of group anticevic <aa353@grace2.grace.hpc.yale.internal>
2019-01-07 16:50:43 -0500

    Fixed QCPreprocessing.sh bug and added cnr_maps flag to DWIPreprocPipelineLegacy.sh
    Former-commit-id: d306395d21b239cfd5513f1bb923046d606a84e3
    Former-commit-id: feb782bea2cd0f721e6f2bf1e00cb1035068e05c

--- bin/SRUN_MNAPSingularity.sh
+++ bin/SRUN_MNAPSingularity.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-CDIR=`pwd`
 TimeStamp=`date +%Y-%m-%d-%H-%M-%S`
-sbatch --output=${HOME}/slurm-mnap-singularity-testrun-${TimeStamp}.out --export=TimeStamp=${TimeStamp},HOME=${HOME} ${CDIR}/SBATCH_MNAPSingularity.sh
+sbatch --output=${HOME}/slurm-mnap-singularity-testrun-${TimeStamp}.out --export=TimeStamp=${TimeStamp},HOME=${HOME} ${TOOLS}/mnaptools/library/bin/SBATCH_MNAPSingularity.sh
 
