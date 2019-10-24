31a2b48152c38765b834c1567bbfa382a8218dbe
Alan Anticevic of group anticevic <aa353@grace2.grace.hpc.yale.internal>
2019-10-24 14:30:24 -0400

    Committing edit to RunTurnkey.sh and cs-wrapper.sh
    Former-commit-id: 53d2ed825f86776892c0f3bb162c8b13e7dbdf2d
    Former-commit-id: 15e09b85fa2c5d1f09cd9a8175908a574b795ce3

--- etc/cs-wrapper.sh
+++ etc/cs-wrapper.sh
@@ -1,2 +1,2 @@
 #!/bin/bash -i
-/opt/qunex/connector/functions/RunTurnkey.sh ${@:1} --xnathost=${XNAT_HOST} --xnatuser=${XNAT_USER} --xnatpass=${XNAT_PASS}
+/opt/qunex/connector/qunex.sh ${@:1} --function='runTurnkey' --xnathost=${XNAT_HOST} --xnatuser=${XNAT_USER} --xnatpass=${XNAT_PASS}
