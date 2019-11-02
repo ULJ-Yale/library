76eaaf6693bb6a01294f2dd7befb72934035c0c0
Alan Anticevic of group anticevic <aa353@transfer-n3.hpc.yale.edu>
2019-11-02 18:30:21 -0400

    Committing edit to qunex.sh and cs-wrapper.sh
    Former-commit-id: 4cc129423c3aa19fa073a2f533f12ffaf41d68db
    Former-commit-id: 7e4ff5d8e2740ad5d90bd0c9868fff13bfaa0b3b

--- etc/cs-wrapper.sh
+++ etc/cs-wrapper.sh
@@ -1,2 +1,68 @@
 #!/bin/bash -i
-/opt/qunex/connector/qunex.sh ${@:1} --function='runTurnkey' --xnathost=${XNAT_HOST} --xnatuser=${XNAT_USER} --xnatpass=${XNAT_PASS}
+#
+#~ND~FORMAT~MARKDOWN~
+#~ND~START~
+#
+# ## COPYRIGHT NOTICE
+#
+# Copyright (C) 2015 Anticevic Lab, Yale University
+# Copyright (C) 2015 MBLAB, University of Ljubljana
+#
+# ## AUTHORS(s)
+#
+# * Alan Anticevic, N3 Division, Yale University
+# * Zailyn Tamayo, N3 Division, Yale University 
+# * Grega Repovs, MBLAB, University of Ljubljana
+#
+# ## PRODUCT
+#
+#  cs-wrapper.sh
+#
+# ## LICENSE
+#
+# * The cs-wrapper.sh = the "Software"
+# * This Software conforms to the license outlined in the Qu|Nex Suite:
+# * https://bitbucket.org/oriadev/qunex/src/master/LICENSE.md
+#
+# ### TODO
+#
+#
+# ## Description 
+#   
+# This script serves as the container service parsing of input for the main Qu|Nex wrapper
+# 
+# ## Prerequisite Installed Software
+#
+# * Qu|Nex Suite
+#
+# ## Prerequisite Environment Variables
+#
+# XNAT environment
+#
+# ### Expected Previous Processing
+# 
+#
+#~ND~END~
+
+# -- String parsed from XNAT Container Service
+CSInputString="${@:1}"
+
+# -- Output folder check
+if [[ -z ${OutputFolder} ]]; then OutputFolder="/gpfs/project/fas/n3/output2"; fi
+
+# -- Full Qu|Nex command
+CSWrapperCmd="$TOOLS/qunex/connector/qunex.sh runTurnkey \
+--xnathost=${XNAT_HOST} \
+--xnatuser=${XNAT_USER} \
+--xnatpass=${XNAT_PASS} \
+--studyfolder=${OutputFolder} \
+${CSInputString}"
+
+echo ""
+echo "Qu|Nex Container Service Parsed Qu|Nex Command:"
+echo "-----------------------------------------------"
+echo ""
+echo $CSWrapperCmd
+echo ""
+
+eval $CSWrapperCmd
