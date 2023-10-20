311b8aef1b072a2be9551ad1375d829f9494a45b
sbrege <samuel.brege@yale.edu>
2023-10-20 16:28:58 -0400

    fix: Added Xnat flag, export variables:

--- etc/xnat/cs_wrapper.sh
+++ etc/xnat/cs_wrapper.sh
@@ -28,9 +28,9 @@
 #
 #~ND~END~
 
-mkdir /output/${XNAT_PROJECT_ID}
-STUDY_FOLDER=/output/${XNAT_PROJECT_ID}
-SESSIONS_FOLDER=${STUDY_FOLDER}/sessions
+mkdir /output/${XNAT_PROJECT}
+export STUDY_FOLDER=/output/${XNAT_PROJECT}
+export SESSIONS_FOLDER=${STUDY_FOLDER}/sessions
 
 CSWrapperCmd="$TOOLS/qunex/bin/qunex.sh create_study \
     --studyfolder=${STUDY_FOLDER}"
@@ -47,21 +47,23 @@ eval $CSWrapperCmd
 curl -k -u ${XNAT_USER_NAME}:${XNAT_PASSWORD} -X GET "${XNAT_HOST_NAME}/data/projects/${XNAT_PROJECT}/resources/QUNEX_PROC/files/${RECIPE_FILENAME}" > ${SESSIONS_FOLDER}/specs/${RECIPE_FILENAME}
 
 #For Xnat testing
-mkdir ${SESSIONS_FOLDER}/specs/xnat
-curl -k -u ${XNAT_USER_NAME}:${XNAT_PASSWORD} -X GET "${XNAT_HOST_NAME}/data/archive/projects/${XNAT_PROJECT}/resources/BASH_SCRIPTS/files?format=zip" > ${SESSIONS_FOLDER}/specs/xnat/BASH_SCRIPTS.zip
-cd ${SESSIONS_FOLDER}/specs/xnat
+export XNAT_SCRIPTS_FOLDER=${SESSIONS_FOLDER}/specs/xnat
+mkdir ${XNAT_SCRIPTS_FOLDER}
+curl -k -u ${XNAT_USER_NAME}:${XNAT_PASSWORD} -X GET "${XNAT_HOST_NAME}/data/archive/projects/${XNAT_PROJECT}/resources/BASH_SCRIPTS/files?format=zip" > ${XNAT_SCRIPTS_FOLDER}/BASH_SCRIPTS.zip
+cd $XNAT_SCRIPTS_FOLDER
 unzip -j BASH_SCRIPTS.zip
 rm BASH_SCRIPTS.zip
 cd $STUDY_FOLDER
 
-RECIPE_FILE=${SESSIONS_FOLDER}/specs/${RECIPE_FILENAME}
-INITIAL_PARAMETERS=${SESSIONS_FOLDER}/specs/${BATCH_PARAMETERS_FILENAME}
-MAPPING=${SESSIONS_FOLDER}/specs/${SCAN_MAPPING_FILENAME}
-BATCH_PARAMETERS=${STUDY_FOLDER}/processing/${BATCH_PARAMETERS_FILENAME}
+export RECIPE_FILE=${SESSIONS_FOLDER}/specs/${RECIPE_FILENAME}
+export INITIAL_PARAMETERS=${SESSIONS_FOLDER}/specs/${BATCH_PARAMETERS_FILENAME}
+export MAPPING=${SESSIONS_FOLDER}/specs/${SCAN_MAPPING_FILENAME}
+export BATCH_PARAMETERS=${STUDY_FOLDER}/processing/${BATCH_PARAMETERS_FILENAME}
 
 CSWrapperCmd="$TOOLS/qunex/bin/qunex.sh run_recipe \
     --recipe_file=${RECIPE_FILE} \
-    --recipe=${RECIPE}"
+    --recipe=${RECIPE}" \
+    --xnat=True"
 
 echo ""
 echo "QuNex Container Service Parsed QuNex Command:"
