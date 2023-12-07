446189728d662c0e4f8e31d8b70c756bd8cbeae6
Samuel Brege <samuel.brege@yale.edu>
2023-12-07 16:10:40 +0000

    feat: Updated xnat wrapper for run_recipe, added 'develop' mode

--- etc/xnat/cs_wrapper.sh
+++ etc/xnat/cs_wrapper.sh
@@ -28,9 +28,14 @@
 #
 #~ND~END~
 
+# -- String parsed from XNAT Container Service
+CSInputString="${@:1}"
+
 mkdir /output/${XNAT_PROJECT}
 export STUDY_FOLDER=/output/${XNAT_PROJECT}
 export SESSIONS_FOLDER=${STUDY_FOLDER}/sessions
+
+#Run create study to setup correct folder structure
 
 CSWrapperCmd="$TOOLS/qunex/bin/qunex.sh create_study \
     --studyfolder=${STUDY_FOLDER}"
@@ -44,15 +49,24 @@ echo ""
 
 eval $CSWrapperCmd
 
+mkdir $SESSIONS_FOLDER/$LABEL
+mkdir $SESSIONS_FOLDER/$LABEL/inbox
+mkdir $SESSIONS_FOLDER/$LABEL/checkpoints
+
 #Pulls in recipe from project level
 curl -k -u ${XNAT_USER}:${XNAT_PASS} -X GET "${XNAT_HOST}/data/projects/${XNAT_PROJECT}/resources/QUNEX_PROC/files/${RECIPE_FILENAME}" > ${SESSIONS_FOLDER}/specs/${RECIPE_FILENAME}
 
-# #For Xnat testing with Qunex, pulls updated scripts into container and replaces old ones
-curl -k -u ${XNAT_USER}:${XNAT_PASS} -X GET "${XNAT_HOST}/data/archive/projects/${XNAT_PROJECT}/resources/QUNEX_SCRIPTS/files?format=zip" > ~/QUNEX_SCRIPTS.zip
-cd ~
-unzip -j QUNEX_SCRIPTS.zip
-bash move.sh
-cd $STUDY_FOLDER
+#For qunex development without updating container, pulls updated scripts into container and replaces old ones
+#New scripts must be placed in a directory called QUNEX_SCRIPTS at the project level
+#Finally, a new script must be created and uploaded to the same location called 'move.sh'
+#In this script you add 'mv' commands to move the updated scripst to the correct location, as well as any other bash commands you want to execute (eg. for wrapper development)
+if [[ $DEVELOP = yes ]]; then
+    curl -k -u ${XNAT_USER}:${XNAT_PASS} -X GET "${XNAT_HOST}/data/archive/projects/${XNAT_PROJECT}/resources/QUNEX_SCRIPTS/files?format=zip" > ~/QUNEX_SCRIPTS.zip
+    cd ~
+    unzip -j QUNEX_SCRIPTS.zip
+    bash move.sh
+    cd $STUDY_FOLDER
+fi
 
 export RECIPE_FILE=${SESSIONS_FOLDER}/specs/${RECIPE_FILENAME}
 export INITIAL_PARAMETERS=${SESSIONS_FOLDER}/specs/${BATCH_PARAMETERS_FILENAME}
@@ -62,7 +76,7 @@ export BATCH_PARAMETERS=${STUDY_FOLDER}/processing/${BATCH_PARAMETERS_FILENAME}
 CSWrapperCmd="$TOOLS/qunex/bin/qunex.sh run_recipe \
     --recipe_file=${RECIPE_FILE} \
     --recipe=${RECIPE} \
-    --xnat=yes"
+    ${CSInputString}"
 
 echo ""
 echo "QuNex Container Service Parsed QuNex Command:"
@@ -73,8 +87,8 @@ echo ""
 
 eval $CSWrapperCmd
 
-# -- String parsed from XNAT Container Service
-CSInputString="${@:1}"
+
+
 
 COM_LOGS_DIR="/output/${XNAT_PROJECT}/processing/logs/comlogs"
 
