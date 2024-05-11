e0a9d532e1dc5ae76c2e606c946957181a67e7e1
Jure Demsar <jure.demsar@fri.uni-lj.si>
2024-05-11 06:53:55 +0000

    fix: changed a check so there are no error in the container

--- etc/cudimot/cuda_11.3/bin/Pipeline_NODDI_Bingham.sh
+++ etc/cudimot/cuda_11.3/bin/Pipeline_NODDI_Bingham.sh
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2e5fd8f489536a6d97266b9c55ef4587637a6e507c406194ebcdaf784ed1037b
-size 8621
+oid sha256:553836d8ce6f64c07b04d73428ce39d716d23c4415c0cb788c72c1553c0de7af
+size 8616
