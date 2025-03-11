89dd245ccdef80d89899dd048d1f4ceed050be49
Jure Demsar <demsarjure@gmail.com>
2025-03-11 09:25:09 +0000

    chore: migration

--- etc/docs/_extras/bash/pre_build.sh
+++ etc/docs/_extras/bash/pre_build.sh
@@ -2,11 +2,9 @@
 
 cd $(dirname $0)
 
-echo "---> Cloning latest wiki version from GitLab"
-
-# Use a project access token with `read_repository` api scope for qunex.wiki
-echo "    ... Cloning from https://token:${GITLAB_QUNEX_TOKEN}@gitlab.qunex.yale.edu/qunex/qunex.wiki.git"
-git clone --depth 1 https://token:${GITLAB_QUNEX_TOKEN}@gitlab.qunex.yale.edu/qunex/qunex.wiki.git ../../wiki
+echo "---> Cloning latest wiki version from GitHub"
+echo "    ... Cloning from https://github.com/ULJ-Yale/qunex.wiki.git"
+git clone --depth 1 https://github.com/ULJ-Yale/qunex.wiki.git ../../wiki
 
 # dev
 # cp -r ../../../../../../qunex.wiki/ ../../wiki  # useful during development
